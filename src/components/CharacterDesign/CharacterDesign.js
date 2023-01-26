5e02adc09f5233019a9995aa1475e0fc7e2ff217
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-01-26 23:47:34 +0600

    chracter design set up done

--- src/components/CharacterDesign/CharacterDesign.js
+++ src/components/CharacterDesign/CharacterDesign.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const CharacterDesign = () => {
     return (
-        <div id='characterDesign' className='my-32 p-10'>
+        <div id='characterDesign' className='pb-20 md:pb-32 px-5 md:px-10 bg-primary text-white text-center'>
+            <h1 className='text-4xl font-semibold mb-5'>Character Design</h1>
+            <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
         </div>
     );
 };
