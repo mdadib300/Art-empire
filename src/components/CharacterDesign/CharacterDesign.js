6c8d079528ebd7a2db5111240d85c50be0f414eb
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-01-26 23:49:26 +0600

    Grid layout set up done

--- src/components/CharacterDesign/CharacterDesign.js
+++ src/components/CharacterDesign/CharacterDesign.js
@@ -5,6 +5,9 @@ const CharacterDesign = () => {
         <div id='characterDesign' className='pb-20 md:pb-32 px-5 md:px-10 bg-primary text-white text-center'>
             <h1 className='text-4xl font-semibold mb-5'>Character Design</h1>
             <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+
+            </div>
         </div>
     );
 };
