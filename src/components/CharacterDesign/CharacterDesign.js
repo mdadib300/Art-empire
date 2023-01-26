e2edbc136bd3e8c4cc5fea9e111e0ed40fd0e2f1
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-01-26 23:52:30 +0600

    Card correction set up done

--- src/components/CharacterDesign/CharacterDesign.js
+++ src/components/CharacterDesign/CharacterDesign.js
@@ -11,13 +11,11 @@ const CharacterDesign = () => {
             <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="Character Design - Rafi's Art Empire" />
+                    </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+                        <h2 className="card-title">Character Design 1</h2>
                     </div>
                 </div>
             </div>
