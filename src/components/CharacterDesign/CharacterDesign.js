0cf32a41ea9c69f5952e2399e829732053775cbb
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-01-28 16:05:58 +0600

    Character design correction done

--- src/components/CharacterDesign/CharacterDesign.js
+++ src/components/CharacterDesign/CharacterDesign.js
@@ -6,16 +6,16 @@ import cd4 from '../../assets/CharacterDesign/4.jpg';
 
 const CharacterDesign = () => {
     return (
-        <div id='characterDesign' className='pb-20 md:pb-32 px-5 md:px-10 bg-primary text-white text-center'>
+        <div id='characterDesign' className='pb-20 md:pb-32 px-5 md:px-10 text-white text-center'>
             <h1 className='text-4xl font-semibold mb-5'>Character Design</h1>
-            <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
+            <p>Sample text</p>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-10'>
                 <div className="card bg-white text-primary">
                     <figure>
                         <img src={cd1} alt="Character Design - Rafi's Art Empire" className='w-full' />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Pori Mori Tori</h2>
+                        <h2 className="card-title">Character Design - 1</h2>
                     </div>
                 </div>
                 <div className="card bg-white text-primary">
@@ -23,7 +23,7 @@ const CharacterDesign = () => {
                         <img src={cd2} alt="Character Design - Rafi's Art Empire" className='w-full' />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Lalshalu</h2>
+                        <h2 className="card-title">Character Design - 2</h2>
                     </div>
                 </div>
                 <div className="card bg-white text-primary">
@@ -31,7 +31,7 @@ const CharacterDesign = () => {
                         <img src={cd3} alt="Character Design - Rafi's Art Empire" className='w-full' />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">GhurMuri</h2>
+                        <h2 className="card-title">Character Design - 3</h2>
                     </div>
                 </div>
                 <div className="card bg-white text-primary">
@@ -39,7 +39,7 @@ const CharacterDesign = () => {
                         <img src={cd4} alt="Character Design - Rafi's Art Empire" className='w-full' />
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">ShurShuri</h2>
+                        <h2 className="card-title">Character Design - 4</h2>
                     </div>
                 </div>
             </div>
